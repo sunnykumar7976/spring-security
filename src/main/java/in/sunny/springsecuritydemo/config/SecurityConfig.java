a76c12c524b7c297af08d4fac7fdd055b0cd4b38
sunnykumar7976 <132023863+sunnykumar7976@users.noreply.github.com>
2025-10-03 22:43:08 -0400

    Added Role Based Authorization

--- src/main/java/in/sunny/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/in/sunny/springsecuritydemo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package in.sunny.springsecuritydemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
--- src/main/java/in/sunny/springsecuritydemo/controller/GreetingsCoontroller.java
+++ src/main/java/in/sunny/springsecuritydemo/controller/GreetingsCoontroller.java
@@ -1,5 +1,6 @@
 package in.sunny.springsecuritydemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingsCoontroller {
 
-    @GetMapping
-    public String Hello(){
+    @GetMapping("/hello")
+    public String sayHello(){
         return "Hello World";
     }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('User')")
+    public String userHello(){
+        return "Hello User";
+    }
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('Admin')")
+    public String adminHello(){
+        return "Hello Sunny";
+    }
+
 }
