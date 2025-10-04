2149ce99426760257a92d70453028da546df7966
sunnykumar7976 <132023863+sunnykumar7976@users.noreply.github.com>
2025-10-03 22:18:25 -0400

    configured in memory Authentication by addming some users details

--- src/main/java/in/sunny/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/in/sunny/springsecuritydemo/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +25,24 @@ public class SecurityConfig {
         // http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user123")
+                .roles("User")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}user123")
+                .roles("User")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}user123")
+                .roles("Admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, admin);
     }
 }
